a2de53f610a2f11bade589ab7baaa91810631e9e
panisara019 <24508@srithatpit.ac.th>
2025-01-17 11:29:06 +0700

    Add files via upload

---
+++ h1.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>-----การแสดงข้อความแบบหัวข้อ-----</p>
+    <div>
+        <h1>HTMLเบื้องต้น(h1)</h1>
+    </div>    
+    <div> 
+    <h1>HTMLเบื้องต้น(h1)</h1>
+    <h2>HTMLเบื้องต้น(h1)</h2>
+    <h3>HTMLเบื้องต้น(h1)</h3>
+    <h4>HTMLเบื้องต้น(h1)</h4>
+    <h5>HTMLเบื้องต้น(h1)</h5>
+    <h6>HTMLเบื้องต้น(h1)</h6>
+    </div>
+    <p>------การสร้างตัวหนา ตัวเอียง การขีดเส้นใต้ข้อความ------</p>
+    <div>
+        <b>ปาณิสรา พาลีย์</b><br>
+        <i>ปาณิสรา พาลีย์</i><br>
+        <u>ปาณิสรา พาลีย์</u><br>
+    </div>
+    <p><b>-----การ link------</b></p>
+    <div>
+        <!--Linkหน้าเดียว--> 
+        <a href = "https://www.google.co.th">เปิด Gooogle</a><br>
+    </div>   
+</body>
+</html>
